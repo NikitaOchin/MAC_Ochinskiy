12099fddf9e64dfdff88559e27e04ce87156c3e2
NikitaOchin <NikitaOchinskiy@gmail.com>
2020-02-24 20:44:31 +0200

    Initial_commit

---
+++ MAC_DLL/MAC_Series.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAC_DLL
+{
+    public class MAC_Series
+    {
+        //Вычисление суммы членов числового ряда Members,
+        //начиная с индекса Initial_Index и заканчивая индексом Last_Index
+
+        public static double Sum_of_Number_Series(
+            int Initial_Index,
+            int Last_index,
+            Func<int,double> Members)
+        {
+            double global_sum = 0.0;
+            for (int k = Initial_Index; k <= Last_index; k++)
+                global_sum += Members(k);
+            return global_sum;
+        }
+
+        public static double Sum_of_Number_Series_A(
+            int Initial_Index, 
+            double Eps,
+            Func<int, double> Members,
+            ref int Final_index)
+        {
+            double ak, sum_1, sum_0 = 0.0; int k = Initial_Index; bool flag;
+            do
+            {
+              ak = Members(k); sum_0 += ak; flag = Math.Abs(ak) >= Eps;
+              if(flag) k++;
+            } while (flag);
+
+            int N = k - Initial_Index + 1; k++;
+            do
+            {
+              sum_1 = Sum_of_Number_Series(k, N+k, Members);
+              sum_0 += sum_1; flag = Math.Abs(sum_1) >= Eps;
+              if (flag) k = k + N + 1;
+            } while (flag);
+            Final_index = k + N; return sum_0;
+        }
+
+        public static double Sum_of_Number_Series_D(
+            int Initial_Index,
+            double Delta,
+            Func<int, double> Members,
+            ref int Final_index)
+        {
+            double ak, sum_1, sum_0 = 0.0; int k = Initial_Index; bool flag;
+            do
+            {
+              ak = Members(k); sum_0 += ak; flag = Math.Abs(ak) >= Delta;
+              if (flag) k++;
+            } while (flag);
+
+            int N = k - Initial_Index + 1; k++;
+            do
+            {
+                sum_1 = Sum_of_Number_Series(k, N + k, Members);
+                sum_0 += sum_1; flag = Math.Abs(sum_1 / sum_0) >= Delta;
+                if (flag) k = k + N + 1;
+            } while (flag);
+            Final_index = k + N; return sum_0;
+        }
+    
+    }
+}
---
+++ MAC_LabWork_1_1/Main_LW_1_1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CLS = MAC_DLL.MAC_Series;
+
+namespace MAC_LabWork_1_1
+{
+    class Main_LW_1_1
+    {
+        static void Main(string[] args)
+        {
+            double True_Sum1 = Math.PI * Math.PI / 8.0 - 1.0;
+            double True_Sum2 = Math.Pow(Math.PI,3.0) / 32.0;
+            int N = 10000, kf = 0; double Eps = 1.0E-9, delta = 1.0E-8;
+
+            double S1_N = CLS.Sum_of_Number_Series(1,N,My_ak);
+            Console.WriteLine("\r\n Summa 1 :");
+            Console.WriteLine($"{N,8}{S1_N,20:F15}\r\n{True_Sum1,28:F15}");
+            
+            double S1_A = CLS.Sum_of_Number_Series_A(1, Eps, My_ak, ref kf);
+            Console.WriteLine($"{kf,8}{S1_A,20:F15}");
+
+            double S1_D = CLS.Sum_of_Number_Series_D(1, delta, My_ak, ref kf);
+            Console.WriteLine($"{kf,8}{S1_D,20:F15}");
+
+            double S2_N = CLS.Sum_of_Number_Series(0, N, My_bk);
+            Console.WriteLine("\r\n Summa 2 :");
+            Console.WriteLine($"{N,8}{S2_N,20:F15}\r\n{True_Sum2,28:F15}");
+
+            double S2_A = CLS.Sum_of_Number_Series_A(0, Eps, My_bk, ref kf);
+            Console.WriteLine($"{kf,8}{S2_A,20:F15}");
+
+            double S2_D = CLS.Sum_of_Number_Series_D(0, delta, My_bk, ref kf);
+            Console.WriteLine($"{kf,8}{S2_D,20:F15}");
+        }
+
+        static double My_ak(int k)
+        {
+            double a = 2.0 * k + 1.0; return 1.0 / a / a;
+        }
+
+        static double My_bk(int k)
+        {
+            double b = 2.0 * k + 1.0;
+            return 1.0 / b / b / b * (k % 2 == 0 ? 1.0 : -1.0);
+        }
+    }
+}
