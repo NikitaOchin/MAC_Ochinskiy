ff1e6ee7ee1d41c9da7648659e203d245536998f
NikitaOchin <NikitaOchinskiy@gmail.com>
2020-02-25 13:53:46 +0200

    Completed 1_1 Lab and CT

--- MAC_CheckTask_1_1/Main_CT_1_1.cs
+++ MAC_CheckTask_1_1/Main_CT_1_1.cs
@@ -3,13 +3,58 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CLS = MAC_DLL.MAC_Series;
 
 namespace MAC_CheckTask_1_1
 {
     class Main_CT_1_1
     {
+        static int N = 12800;
+        static double a = 1.77, b = -2.82, c = 0.81, d = 2.42;
+        static double eps = 1.0E-9, delta = 1.0E-8;
+
         static void Main(string[] args)
         {
+            int kf = 0;
+            double S1_N = CLS.Sum_of_Number_Series(0,N,Series_SN);
+            Console.WriteLine($"{N,8}{S1_N,20:F10}\r\n");
+            double S1_A = CLS.Sum_of_Number_Series_A(-1,eps, Series_S1, ref kf);
+            Console.WriteLine($"{kf,8}{S1_A,20:F10}\r\n");
+            double S1_D = CLS.Sum_of_Number_Series_D(1, delta, Series_S2,ref kf);
+            Console.WriteLine($"{kf,8}{S1_D,20:F10}\r\n");
+
+            N = 10000;
+            a = 1.20; b = -2.10; c = -0.50; d = 1.40;
+            double S1_NT = CLS.Sum_of_Number_Series(0, N, Series_SN);
+            Console.WriteLine($"{N,8}{S1_NT,20:F10}\r\n");
+            double S1_AT = CLS.Sum_of_Number_Series_A(-1, eps, Series_S1, ref kf);
+            Console.WriteLine($"{kf,8}{S1_AT,20:F10}\r\n");
+            double S1_DT = CLS.Sum_of_Number_Series_D(1, delta, Series_S2, ref kf);
+            Console.WriteLine($"{kf,8}{S1_DT,20:F10}\r\n");
+
+
         }
+
+        public static double Series_SN(int k) 
+        {
+            double s1 = Math.Pow(k + 1, 1.0/4.0);
+            double s2 = 3.0 * k * k - 1;
+            double s3 = Math.Sqrt(k) + 3;
+            return s1 / s2 / s3;
+        }
+        public static double Series_S1(int k) 
+        {
+            double s1 = Math.Sqrt(a+k) + Math.Sqrt(k-b);
+            double s2 = Math.Pow(5.0 * k + 2.0 * a, 2);
+            double s3 = b * k + 1.0;
+            return s1 / s2 / s3;
+        }
+        public static double Series_S2(int k) 
+        {
+            double s1 = Math.Pow(k,1.5) + c;
+            double s2 = 2.0 * k - d + 1;
+            return 1.0 / s1 / s2 * (k % 2 == 0 ? 1.0 : -1.0);
+        }
+
     }
 }
--- MAC_LabWork_1_1/Main_LW_1_1.cs
+++ MAC_LabWork_1_1/Main_LW_1_1.cs
@@ -34,6 +34,25 @@ namespace MAC_LabWork_1_1
 
             double S2_D = CLS.Sum_of_Number_Series_D(0, delta, My_bk, ref kf);
             Console.WriteLine($"{kf,8}{S2_D,20:F15}");
+
+            Console.WriteLine("\r\n HOME_WORK");
+            double TrueSum1_H = 0.3794715778;
+            double TrueSum2_H = 3.0/4.0*(1.0-2.0*Math.PI/Math.Sqrt(27));
+
+            N = 18600;
+            double S1_H = CLS.Sum_of_Number_Series(1, N, My_ck);
+            int T_N = 17500;
+            double S1_H_T = CLS.Sum_of_Number_Series(1, T_N, My_ck);
+            Console.WriteLine("\r\n Summa 1 for home :");
+            Console.WriteLine($"{N,8}{S1_H,20:F10}\r\n");
+            bool check = Math.Round(S1_H_T,10) == Math.Round(TrueSum1_H,10);
+            Console.WriteLine($"{T_N,8}{S1_H_T,20:F10}\r\n{TrueSum1_H,28:F10}\r\n{check,20}");
+
+            double S2_H_A = CLS.Sum_of_Number_Series_A(1, Eps, My_dk, ref kf);
+            Console.WriteLine("\r\n Summa 2 for home :");
+            Console.WriteLine($"{kf,8}{S2_H_A,20:F10}\r\n{TrueSum2_H,28:F10}");
+
+            Console.ReadLine();
         }
 
         static double My_ak(int k)
@@ -45,6 +64,16 @@ namespace MAC_LabWork_1_1
         {
             double b = 2.0 * k + 1.0;
             return 1.0 / b / b / b * (k % 2 == 0 ? 1.0 : -1.0);
+        }
+
+        static double My_ck(int k)
+        {
+            return 1.0 / k / (3.0 * k + 2.0);
+        }
+
+        static double My_dk(int k)
+        {
+            return 1.0 / k / (3.0 * k + 2) * (k % 2 == 0 ? 1.0 : -1.0);
         }
     }
 }
