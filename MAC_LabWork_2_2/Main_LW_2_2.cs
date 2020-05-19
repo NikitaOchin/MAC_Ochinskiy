78925756c6971e2f227f694a5040316769eac896
NikitaOchin <NikitaOchinskiy@gmail.com>
2020-05-19 16:16:05 +0300

    Update Main_LW_2_2.cs

--- MAC_LabWork_2_2/Main_LW_2_2.cs
+++ MAC_LabWork_2_2/Main_LW_2_2.cs
@@ -29,11 +29,12 @@ namespace MAC_LabWork_2_2
             double error = MAC_Algebra.Error_of_SLAE(A, X, b);
             SW.WriteLine($"\r\n Error = {error,10:E1} iterations = {K}");
 
+            //HOMEWORK
             string file_h = "LW_2_2_Ab_3_v02.txt"; int Variant_h = 2;
             SW.WriteLine($"\r\n HOME_WORK Ochinskiy \r\n {file_h} Variant = {Variant_h}");
 
             Matrix.Read(file_h, out Matrix A_h, out Vector b_h, out int n_h);
-            SW.Write(Matrix.Print(A, b, true, 3, 3, "Matrix Ab"));
+            SW.Write(Matrix.Print(A_h, b_h, true, 3, 3, "Matrix Ab"));
 
             Vector X_h = MAC_Algebra.Method_Zeidela(A_h, b_h, 1.0E-8, out int K_h);
 
